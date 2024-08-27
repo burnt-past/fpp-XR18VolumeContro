ae1dd994395ff45fafc97e4e5bf88b7ffe3c7fd8
Alexander Woolum <alexanderwoolum@gmail.com>
2024-08-26 21:35:25 -0700

    Some changes

--- output.php
+++ output.php
@@ -1,18 +1,41 @@
 <?php
-    $pluginName = "XR18VolumeControl";
-    $MIDI_DEVICE = "hw:2,0,0";
-    $CHANNEL_FADER_CC = "00";
-    function convertVolumeToMidi($volume) {
-        return dechex(intval($volume * 127 / 100));
+
+$pluginName = "XR18VolumeControl";
+$MIDI_DEVICE = "hw:2,0,0";  // Set this to your actual MIDI device path
+$CHANNEL_FADER_CC = "00";  // MIDI CC number for Channel 1 fader
+
+function convertVolumeToMidi($volume)
+{
+    return dechex(intval($volume * 127 / 100));
+}
+
+function sendMidiCommand($volume)
+{
+    global $MIDI_DEVICE, $CHANNEL_FADER_CC;
+
+    $midiValue = convertVolumeToMidi($volume);
+    $command = "amidi -p $MIDI_DEVICE -S 'B0 $CHANNEL_FADER_CC $midiValue'";
+
+    // Execute the command
+    exec($command);
+}
+
+function getFppVolume()
+{
+    $api_url = "http://localhost/api/fppd/volume";
+    $json = file_get_contents($api_url);
+    $data = json_decode($json, true);
+    if ($data['Status'] == 'OK') {
+        return intval($data['volume']);
     }
-    function sendMidiCommand($volume) {
-        global $MIDI_DEVICE, $CHANNEL_FADER_CC;
-        $midiValue = convertVolumeToMidi($volume);
-        $command = "amidi -p $MIDI_DEVICE -S 'B0 $CHANNEL_FADER_CC $midiValue'";
-        exec($command);
-    }
-    if (isset($_POST['volume'])) {
-        $volume = intval($_POST['volume']);
-        sendMidiCommand($volume);
-    }
-    ?>
+    return null;
+}
+
+// Fetch the current volume from FPP API
+$volume = getFppVolume();
+
+if ($volume !== null) {
+    sendMidiCommand($volume);
+} else {
+    error_log("Failed to retrieve volume from FPP API.");
+}
